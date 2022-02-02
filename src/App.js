78712736090edaa7fdd894b2670f01a170a27882
unknown <maniplek81@gmail.com>
2022-02-02 22:16:50 +0200

    setting up reminder

--- src/App.js
+++ src/App.js
@@ -33,11 +33,21 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+//Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks   .map((task) => task.id === id
+  ? { ...task, reminder:
+  !task.reminder } : task
+  )
+  )
+}
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : (
+      {tasks.length > 0 ? <Tasks tasks={tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder} /> : (
         'No Task to Show')}
     </div>
   );
